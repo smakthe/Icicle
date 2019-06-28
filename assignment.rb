1b03b8c7b24521e21860b11f5e7a9e042a61c8a6
smakthe <somakchakraborty@Somaks-MacBook-Air.local>
2019-06-28 07:46:48 +0530

    create dictionary words with corresponding numbers pairs

--- assignment.rb
+++ assignment.rb
@@ -4,3 +4,15 @@ File.open("dictionary.txt", "r") do |f|
 		dictionary_list << i
 	end
 end
+
+dictionary_hash = {}
+dictionary_list.each do |letter|
+	dictionary_hash[letter] = letter.gsub(/(A|B|C)/, "2")
+																	.gsub(/(D|E|F)/, "3")
+																	.gsub(/(G|H|I)/, "4")
+																	.gsub(/(J|K|L)/, "5")
+																	.gsub(/(M|N|O)/, "6")
+																	.gsub(/(P|Q|R|S)/, "7")
+																	.gsub(/(T|U|V)/, "8")										 
+																	.gsub(/(W|X|Y|Z)/, "9")
+end
